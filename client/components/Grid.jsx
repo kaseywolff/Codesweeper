43d510b4191f487e28dfa8c7cf58d1b93b616b26
kaseywolff <kaseywolff5@gmail.com>
2023-04-05 14:58:03 -0500

    before major grid changes

--- client/components/Grid.jsx
+++ client/components/Grid.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Row from './Row.jsx';
 import Square from './Square.jsx';
 
 import mineGenerator from '../functions/mines.js';
@@ -9,13 +8,11 @@ class Grid extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      // grid needs to be an array
       grid: {},
       gameOver: false,
       mineCount: 0,
       mines: [],
-      isMine: false,
-      isRevealed: false,
-      neighboringMines: 0,
     };
     this.handleClick = this.handleClick.bind(this);
     this.freshGrid = this.freshGrid.bind(this);
@@ -31,29 +28,46 @@ class Grid extends Component {
   freshGrid() {
     const mineArr = mineGenerator();
     const grid = {};
+    // create mine squares/ square coordinates
     for (let i = 0; i < 9; i++) {
       for (let j = 0; j < 9; j++) {
         let loc = `r${i}c${j}`;
         if (mineArr.includes(loc)) {
-          grid[`r${i}c${j}`] = '*';
+          grid[`r${i}c${j}`] = '*'
+          // grid[`r${i}c${j}`] = {
+          //   isRevealed: false,
+          //   isMine: true,
+          //   value: null,
+          // }
         }else {
           grid[`r${i}c${j}`] = 0;
+          // grid[`r${i}c${j}`] = {
+          //   isRevealed: false,
+          //   isMine: false,
+          //   value: 0,
+          // }
         }
       }
     }
-    // console.log('Grid Console: ', grid)
-    // console.log('Mine Array: ', mineArr)
-    // checkForMines(currLoc, mineArr, grid)
-    for(const key in grid) {
-      // console.log(mineArr)
-      let newVal = checkForMines(key, mineArr, grid)
-      console.log('key: ', key, 'newVal: ', newVal)
-      grid[key] = newVal;
+
+    // console.log('grid: ', grid)
+    // console.log(Object.keys(grid))
+    // check for mines and increment each square
+    for(let coordinateKey in grid) {
+      // let info = grid[coordinateKey]
+      // console.log('coorkey: ', coordinateKey)
+      // console.log('info: ', info)
+      // console.log('grid[key]: ', grid[coordinateKey][value])
+
+      let newVal = checkForMines(coordinateKey, mineArr, grid)
+      grid[coordinateKey] = newVal;
+      // grid[coordinateKey][value] = newVal;
+
     }
 
 
     this.setState({
-      grid,
+      grid: grid,
       gameOver: false,
       mineCount: mineArr.length,
       mines: mineArr,
@@ -95,31 +109,42 @@ class Grid extends Component {
     const squares = [];
     for (let i = 0; i < 9; i++) {
       for (let j = 0; j < 9; j++) {
-        if (this.state.mines.includes(`r${i}c${j}`)) {
-          squares.push(
-            <Square
-              key={`${i},${j}`}
-              row={i}
-              col={j}
-              grid={this.state.grid}
-              isMine={true}
-              isRevealed={false}
-              handleClick={this.handleClick}
-            />
-          )
-        }else {
-          squares.push(
-            <Square
-              key={`${i},${j}`}
-              row={i}
-              col={j}
-              grid={this.state.grid}
-              isMine={false}
-              isRevealed={false}
-              handleClick={this.handleClick}
-            />
-          )
-        }
+        // if (this.state.mines.includes(`r${i}c${j}`)) {
+        //   squares.push(
+        //     <Square
+        //       key={`${i},${j}`}
+        //       row={i}
+        //       col={j}
+        //       grid={this.state.grid}
+        //       isMine={true}
+        //       isRevealed={false}
+        //       handleClick={this.handleClick}
+        //     />
+        //   )
+        // }else {
+        //   squares.push(
+        //     <Square
+        //       key={`${i},${j}`}
+        //       row={i}
+        //       col={j}
+        //       grid={this.state.grid}
+        //       isMine={false}
+        //       isRevealed={false}
+        //       handleClick={this.handleClick}
+        //     />
+        //   )
+        // }
+        squares.push(
+          <Square
+            key={`${i},${j}`}
+            row={i}
+            col={j}
+            grid={this.state.grid}
+            // isMine={this.state.grid[`r${i}c${j}`].isMine}
+            // isRevealed={this.state.grid[`r${i}c${j}`].isRevealed}
+            handleClick={this.handleClick}
+          />
+        )
       };
     }
     console.log(this.state)
--- client/functions/checkForMines.js
+++ client/functions/checkForMines.js
@@ -11,168 +11,22 @@
 //   // row - 1, col + 1
 
 
-// function checkForMines(currLoc, mineArr) {
-//   if (mineArr.includes(currLoc)) return '*';
-//   // console.log(currLoc);
-//   // console.log(mineArr);
-//   // console.log(grid)
-//   const row = Number(currLoc[1])
-//   const col = Number(currLoc[3])
-//   // console.log(row)
-//   // console.log(col)
-
-  
-//   let mineCount = 0;
-
-//   let rowCheck = -1;
-//   let colCheck = -1;
-//   let rowMax = 8; // CHANGE TO 8 for 9 rows (index)
-//   let colMax = 8; // CHANGE TO 8
-//   let rowStop = 1;
-//   let colStop = 1;
-
-//   if (row === 0) {
-//     rowCheck = 0
-//   }
-//   if (row === rowMax) {
-//     rowStop = 0;
-//   }
-//   if (col === 0) {
-//     colCheck = 0
-//   }
-//   if (col === colMax) {
-//     colStop = 0;
-//   }
-
-//   for (let r = rowCheck; r <= rowStop; r++) {
-//     // console.log(rowCheck)
-//     // console.log(rowStop)
-//     for (let c = colCheck; c <= colStop; c++) {
-//       let checkLoc = `r${r}c${c}`
-//       // console.log(checkLoc)
-//       if (mineArr.includes(checkLoc)) {
-//         mineCount++
-//       }
-//     }
-//   }
-
-//   return mineCount
-// }
-
-// // const mineArrTest = ["r1c0"]
-
-// // current location will always be passed in in the form 'r#c#'
-
-// // const testLoc = 'r1c2'
-
-// // const testGrid = {
-// //   r0c0: 0,
-// //   r0c1: 0,
-// //   r0c2: '*',
-// //   r1c0: '*',
-// //   r1c1: 0,
-// //   r1c2: 0,
-// //   r2c0: 0,
-// //   r2c1: 0,
-// //   r2c2: 0,
-// // }
-
-// /*
-//   [0 0 *]
-//   [* 0 0]
-//   [0 0 0]
-// */
-
-// // console.log(checkForMines(testLoc, mineArrTest, testGrid))
-
 function checkForMines(location, mineArr, grid) {
   if (mineArr.includes(location)) return '*';
   let rowLoc = Number(location[1])
-  console.log('rowLoc: ', rowLoc)
+  // console.log('rowLoc: ', rowLoc)
   let colLoc = Number(location[3])
-  console.log('colLoc: ', colLoc)
+  // console.log('colLoc: ', colLoc)
+
+  // console.log('cfm function location: ', location)
+  // console.log('cfm function grid: ', grid)
 
   let mineCount = 0;
 
-  // // top left corner
-  // if (rowLoc === 0 && colLoc === 0) {
-  //   // check to the right
-  //   let checkRight = 'r0c1';
-  //   let checkDownRight = 'r1c1';
-  //   let checkDown = 'r1c0';
-  //   if (mineArr.includes(checkRight)) {
-  //     mineCount++
-  //   }
-  //   if (mineArr.includes(checkDownRight)) {
-  //     mineCount++
-  //   }
-  //   if (mineArr.includes(checkDown)) {
-  //     mineCount++
-  //   }
-  // }
-
-  //  // top right corner
-  //  if (rowLoc === 0 && colLoc === 8) {
-  //   // check to the right
-  //   let checkLeft = 'r0c7';
-  //   let checkDownLeft = 'r1c7';
-  //   let checkDown = 'r1c8';
-  //   if (mineArr.includes(checkLeft)) {
-  //     mineCount++
-  //   }
-  //   if (mineArr.includes(checkDownLeft)) {
-  //     mineCount++
-  //   }
-  //   if (mineArr.includes(checkDown)) {
-  //     mineCount++
-  //   }
-  // }
-
-  //  // bottom left corner
-  //  if (rowLoc === 8 && colLoc === 0) {
-  //   // check to the right
-  //   let checkRight = 'r8c1';
-  //   let checkUpRight = 'r7c1';
-  //   let checkUp = 'r7c0';
-  //   if (mineArr.includes(checkRight)) {
-  //     mineCount++
-  //   }
-  //   if (mineArr.includes(checkUpRight)) {
-  //     mineCount++
-  //   }
-  //   if (mineArr.includes(checkUp)) {
-  //     mineCount++
-  //   }
-  // }
-
-  // // bottom right corner
-  // if (rowLoc === 8 && colLoc === 8) {
-  //   // check to the right
-  //   let checkLeft = 'r8c7';
-  //   let checkUpLeft = 'r7c7';
-  //   let checkUp = 'r7c8';
-  //   if (mineArr.includes(checkLeft)) {
-  //     mineCount++
-  //   }
-  //   if (mineArr.includes(checkUpLeft)) {
-  //     mineCount++
-  //   }
-  //   if (mineArr.includes(checkUp)) {
-  //     mineCount++
-  //   }
-  // }
-
-  // // top row
-  // if (rowLoc === 0) {
-  //   let checkLeft = colLoc - 1;
-  //   let checkRight = rowLoc + 1;
-  //   let checkDownLeft = colLoc
-  // }
-
-  // middle squares
   for (let r = -1; r < 2; r++) {
     for (let c = -1; c < 2; c++) {
       let locCheck = `r${rowLoc + r}c${colLoc + c}`
+      // console.log('grid loc check: ', locCheck, grid[locCheck])
       if (grid[locCheck]) {
         if (mineArr.includes(locCheck)) {
           mineCount++
@@ -186,3 +40,23 @@ function checkForMines(location, mineArr, grid) {
 }
 
 export default checkForMines;
+
+
+// let data = [];
+// //will map through height(y) 
+// for (let i = 0; i < 2; i++) {
+//   //and push our data values to it
+//   data.push([]);
+//   //will map through width(x) 
+//   for (let j = 0; j < 2; j++) {
+//     //and hide everything at first (we make a clean board)
+//     data[i][j] = {
+//       x: i,
+//       y: j,
+//       isDog: false,
+//     }
+//   }
+// }
+
+// console.log(data)
+
