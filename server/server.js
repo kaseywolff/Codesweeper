439929448d74ec705307420d90283bb987e32a8e
kaseywolff <kaseywolff5@gmail.com>
2023-04-06 13:45:03 -0500

    pre adding mongoose

--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 
+
+const userController = require('./controllers/userController');
+// const cookieController = require('./controllers/cookieController');
+const sessionController = require('./controllers/sessionController');
+const { isLoggedIn } = require('./controllers/sessionController');
+
 const PORT = 3000;
 
 const app = experss();
@@ -13,6 +19,12 @@ app.use(express.urlencoded());
 
 app.use('/', express.static(path.resolve(__dirname, '../client')));
 
+app.get('/',
+  (req, res) => {
+    res.sendFile(path.resolve(_dirname, './index.html'))
+  }
+)
+
 /**
 * signup
 */
@@ -21,6 +33,59 @@ app.get('/signup',
     res.sendFile(path.resolve(__dirname, './signup.html'));
   }
 );
+
+app.post('/signup', 
+  userController.createUser, 
+  // add a 'secret' cookie
+  cookieController.setSSIDCookie,
+  // start session when user signs up
+  sessionController.startSession,
+  (req, res) => {
+  // what should happen here on successful sign up?
+  // trying to redirect if duplicate username
+  // if (res.locals.redirect === 'signup') {
+  //   return res.redirect('/signup')
+  // }
+    res.redirect('/secret');
+  }
+);
+
+/**
+* login
+*/
+app.post('/login', 
+  userController.verifyUser, 
+  cookieController.setSSIDCookie,
+  sessionController.startSession,
+  (req, res) => {
+  // what should happen here on successful log in?
+    if (res.locals.redirect === 'signup') {
+      return res.redirect('/signup');
+    } else if (res.locals.redirect === 'success') {
+      return res.redirect('/secret')
+    } else res.status(404).send('Not found')
+  }
+);
+
+/**
+* Authorized routes
+*/
+app.get('/game', 
+  // put session controller is loggin in here
+  sessionController.isLoggedIn,
+  // check res.locals to see if we can go to secret page
+  // if we can, continue, otherwise, send to signup
+  (req, res) => {
+  // usre redirected to signup page if they don't have ssid cookie
+    if (res.locals.redirect === 'false') {
+      res.sendFile(path.resolve(__dirname, '../client/secret.html'));
+    } else if (res.locals.redirect === 'true') {
+      return res.redirect('/signup')
+    } else {
+      return res.status(404).send('Not found')
+    }
+
+  });
 
 
 
