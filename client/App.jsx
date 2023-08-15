a8c76195085142f7f9b3b5a1256dfbcc0b5f6c6f
kaseywolff <kaseywolff5@gmail.com>
2023-08-15 15:42:19 -0500

    nav bar component

--- client/App.jsx
+++ client/App.jsx
@@ -2,17 +2,14 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 import Game from './components/Game.jsx';
+import NavBar from './components/NavBar.jsx';
 
 class App extends Component {
   render() {
     return (
       <div id="app">
-        <nav className='navBar'>
-          <button className='navLink'>Create Account</button>
-          <div></div>
-          <button className='navLink'>Login</button>
-        </nav>
-        <Game/>
+        {/* <NavBar /> */}
+        <Game />
         {/* leaderboard */}
         {/* personal best times */}
       </div>
---
+++ client/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <nav className='navBar'>
+      <button className='navLink'>Create Account</button>
+      <div></div>
+      <button className='navLink'>Login</button>
+    </nav>
+  )
+};
+
+export default NavBar;
