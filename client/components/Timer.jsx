f7c8483ab5ece5a2b7f61370389227b8a7f272f4
kaseywolff <kaseywolff5@gmail.com>
2023-08-31 09:56:48 -0500

    timer runs past 60 seconds

--- client/components/Timer.jsx
+++ client/components/Timer.jsx
@@ -32,7 +32,7 @@ function Timer(props) {
 
   return(
     <div id='timer'>
-        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}</span>
+        <span>{("0" + Math.floor((time / 1000))).slice(-2)}</span>
     </div>
   );
 };
