c24e8c32099f39616775d7f8130f73a91174a94c
kaseywolff <kaseywolff5@gmail.com>
2023-11-27 16:54:26 -0600

    centered header and game, adjusts properly with viewport size

--- client/components/Game/Game.jsx
+++ client/components/Game/Game.jsx
@@ -1,25 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Board from './Board.jsx';
-import NavBar from '../NavBar.jsx';
-import Level from '../Level.jsx';
 
 const Game = ({ selectedLevel }) => {
-  // const [selectedLevel, setSelectedLevel] = useState('intermediate');
-  // const [showLevelOptions, setShowLevelOptions] = useState(false);
-
-  // const toggleLevelPopup = () => {
-  //   setShowLevelOptions(!showLevelOptions);
-  // };
 
   return (
     <div id="game">
-      {/* <NavBar onLevelButtonClick={toggleLevelPopup} />
-      {showLevelOptions && <Level selectedLevel={selectedLevel} onLevelChange={setSelectedLevel} />} */}
       <h2>Codesweeper</h2>
-      <Board 
-        selectedLevel={selectedLevel} 
-        // onLevelChange={setSelectedLevel} 
-      />
+      <Board selectedLevel={selectedLevel} />
     </div>
   );
 };
