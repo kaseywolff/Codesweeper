fa1fa961aa526da3ebbf49455f6e5f4bec1f6812
kaseywolff <kaseywolff5@gmail.com>
2023-12-06 15:42:35 -0600

    working on nav bar styling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,20 +6,24 @@ import '../scss/nav-bar.scss';
 export default function NavBar({ onLevelButtonClick }: NavBarProps): JSX.Element {
   return (
     <nav className='navBar'>
-      <Link to='/' id='home-button' className='navLink'>
-        <div className='logo-div'>
-          <div>💻</div>
-          <div className='play-binary'>
+      <Link to='/' id='home-button' >
+        <button className='logo-div navLink'>
+          {/* <div className='computer'>💻</div> */}
+          {/* <div className='play-binary'> */}
             <div className='binary'>01110000 01101100</div>
             <div className='binary'>01100001 01111001</div>
-          </div>
-        </div>
+          {/* </div> */}
+        </button>
       </Link>
+
       <button id='level-button' className='navLink' onClick={onLevelButtonClick}>
         Level
       </button>
-      <Link to='/highscores' id='highscores-button' className='navLink'>
-        HighScores
+
+      <Link to='/highscores' id='highscores-button'>
+        <button className='navLink'>
+          HighScores
+        </button>
       </Link>
       {/* <button className='navLink'>Login</button> */}
     </nav>
