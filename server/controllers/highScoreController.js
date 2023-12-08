f78d71f058a669847cfb03437835bd4624e671cd
kaseywolff <kaseywolff5@gmail.com>
2023-12-07 21:38:10 -0600

    update high score controller for dynamic dummy data fetch

--- server/controllers/highScoreController.js
+++ server/controllers/highScoreController.js
@@ -6,12 +6,24 @@ const highScoreController = {};
 highScoreController.getHighScores = async (req, res, next) => {
   try {
     const level = req.params.level;
-    const highScores = await highScoreModel.query(level);
+
+    // map level to corresponding json dummy data file
+    const levelJsonFiles = {
+      beginner: 'beginner.json',
+      intermediate: 'intermediate.json',
+      expert: 'expert.json',
+    };
+
+    // fetch high scores from corresponding json dummy data file
+    const fileName = levelJsonFiles[level];
+    const highScores = await highScoreModel.query(fileName);
+
     res.json(highScores);
+    next();
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
-  }
+  };
 };
 
 module.exports = highScoreController;
