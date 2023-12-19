88fc279de422d49703de9a9af5a903ac97390012
kaseywolff <kaseywolff5@gmail.com>
2023-12-19 11:33:41 -0600

    fixed 'check high score' to return true when there are < 5 high scores

--- src/logic/checkHighScore.js
+++ src/logic/checkHighScore.js
@@ -1,22 +1,26 @@
 export default function checkHighScore(time, selectedLevel) {
+  let isTop5 = false;
   const highScoreResults = {
-    isTop5: false,
+    isTop5: isTop5,
     data: {},
   };
   // fetch current high scores
-  return fetch(`/api/highscores/${selectedLevel}`)
+  return fetch(`http://localhost:3000/api/highscores/${selectedLevel}`)
   .then(res => res.json())
   .then(currentHighScores => {
     const userTime = time/1000; // time in seconds
     highScoreResults.data = currentHighScores;
     
     // check if user's time is in the top 5
-    const isTop5 = currentHighScores.some(score => {
-      return userTime < score.time;
-    });
-    highScoreResults.isTop5 = isTop5;
-
-
+    console.log('current hs', currentHighScores)
+    if (currentHighScores.length >= 5) {
+      isTop5 = currentHighScores.some(score => {
+        return userTime < score.time;
+      });
+      highScoreResults.isTop5 = isTop5;
+    }else {
+      highScoreResults.isTop5 = true
+    };
     return highScoreResults;
   })
   .catch(error => {
